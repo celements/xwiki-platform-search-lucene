52eff3a4ddfadf24929c9b22e79525ab74bb573c
Fabian Pichler <fabian.pichler@synventis.com>
2013-07-09 09:53:54 +0200

    add logging to getRelevantResults if results are skipped

--- src/main/java/com/xpn/xwiki/plugin/lucene/SearchResults.java
+++ src/main/java/com/xpn/xwiki/plugin/lucene/SearchResults.java
@@ -38,214 +38,214 @@ import com.xpn.xwiki.web.Utils;
 /**
  * Container for the results of a search.
  * <p>
- * This class handles paging through search results and enforces the xwiki rights management by only returning search
- * results the user executing the search is allowed to view.
+ * This class handles paging through search results and enforces the xwiki
+ * rights management by only returning search results the user executing the
+ * search is allowed to view.
  * </p>
  * 
  * @version $Id: 111c8d01d68749e3b4214a49238963ab10b70445 $
  */
-public class SearchResults extends Api
-{
-    private final XWiki xwiki;
+public class SearchResults extends Api {
+  private final XWiki xwiki;
 
-    private final Searcher searcher;
+  private final Searcher searcher;
 
-    private final TopDocsCollector< ? extends ScoreDoc> results;
+  private final TopDocsCollector<? extends ScoreDoc> results;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SearchResults.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(SearchResults.class);
 
-    private List<SearchResult> relevantResults;
+  private List<SearchResult> relevantResults;
 
-    /**
-     * @param results Lucene search results
-     * @param xwiki xwiki instance for access rights checking
-     */
-    SearchResults(TopDocsCollector< ? extends ScoreDoc> results, Searcher searcher, XWiki xwiki,
-        XWikiContext context)
-    {
-        super(context);
+  /**
+   * @param results
+   *          Lucene search results
+   * @param xwiki
+   *          xwiki instance for access rights checking
+   */
+  SearchResults(TopDocsCollector<? extends ScoreDoc> results, Searcher searcher,
+      XWiki xwiki, XWikiContext context) {
+    super(context);
 
-        this.results = results;
-        this.searcher = searcher;
-        this.xwiki = xwiki;
-    }
+    this.results = results;
+    this.searcher = searcher;
+    this.xwiki = xwiki;
+  }
 
-    private List<SearchResult> getRelevantResults()
-    {
-        if (this.relevantResults == null) {
-            this.relevantResults = new ArrayList<SearchResult>();
-            TopDocs docs = this.results.topDocs();
+  private List<SearchResult> getRelevantResults() {
+    if (this.relevantResults == null) {
+      this.relevantResults = new ArrayList<SearchResult>();
+      TopDocs docs = this.results.topDocs();
 
-            for (int i = 0; i < docs.scoreDocs.length; i++) {
-                try {
-                    SearchResult result =
-                        new SearchResult(this.searcher.doc(docs.scoreDocs[i].doc), docs.scoreDocs[i].score, this.xwiki);
+      for (int i = 0; i < docs.scoreDocs.length; i++) {
+        try {
+          SearchResult result = new SearchResult(this.searcher.doc(docs.scoreDocs[i].doc),
+              docs.scoreDocs[i].score, this.xwiki);
 
-                    if (result.isWikiContent()) {
-                        String prefixedFullName =
-                            ((EntityReferenceSerializer<String>)
-                                Utils.getComponent(EntityReferenceSerializer.class))
-                                .serialize(result.getDocumentReference());
-                        if (this.xwiki.exists(result.getDocumentReference())
-                            && this.xwiki.hasAccessLevel("view", this.context.getUser(), prefixedFullName)) {
-                            this.relevantResults.add(result);
-                        }
-                    }
-                } catch (Exception e) {
-                    LOGGER.error("Error getting search result", e);
-                }
+          if (result.isWikiContent()) {
+            String prefixedFullName = ((EntityReferenceSerializer<String>
+                ) Utils.getComponent(EntityReferenceSerializer.class)).serialize(
+                    result.getDocumentReference());
+            if (this.xwiki.exists(result.getDocumentReference())
+                && this.xwiki.hasAccessLevel("view", this.context.getUser(),
+                    prefixedFullName)) {
+              this.relevantResults.add(result);
+            } else {
+              LOGGER.debug("skipping wrong type [" + result.getType() + "] for result ["
+                  + result.getDocumentReference() + "].");
             }
+          } else {
+            LOGGER.debug("skipping because no.");
+          }
+        } catch (Exception e) {
+          LOGGER.error("Error getting search result", e);
         }
-
-        return this.relevantResults;
+      }
     }
 
-    /**
-     * @return true when there are more results than currently displayed.
-     */
-    public boolean hasNext(String beginIndex, String items)
-    {
-        final int itemCount = Integer.parseInt(items);
-        final int begin = Integer.parseInt(beginIndex);
+    return this.relevantResults;
+  }
 
-        return begin + itemCount - 1 < getRelevantResults().size();
+  /**
+   * @return true when there are more results than currently displayed.
+   */
+  public boolean hasNext(String beginIndex, String items) {
+    final int itemCount = Integer.parseInt(items);
+    final int begin = Integer.parseInt(beginIndex);
+
+    return begin + itemCount - 1 < getRelevantResults().size();
+  }
+
+  /**
+   * @return true when there is a page before the one currently displayed, that
+   *         is, when <code>beginIndex > 1</code>
+   */
+  public boolean hasPrevious(String beginIndex) {
+    return Integer.parseInt(beginIndex) > 1;
+  }
+
+  /**
+   * @return the value to be used for the firstIndex URL parameter to build a
+   *         link pointing to the next page of results
+   */
+  public int getNextIndex(String beginIndex, String items) {
+    final int itemCount = Integer.parseInt(items);
+    final int resultcount = getRelevantResults().size();
+    int retval = Integer.parseInt(beginIndex) + itemCount;
+
+    return retval > resultcount ? (resultcount - itemCount + 1) : retval;
+  }
+
+  /**
+   * @return the value to be used for the firstIndex URL parameter to build a
+   *         link pointing to the previous page of results
+   */
+  public int getPreviousIndex(String beginIndex, String items) {
+    int retval = Integer.parseInt(beginIndex) - Integer.parseInt(items);
+
+    return 0 < retval ? retval : 1;
+  }
+
+  /**
+   * @return the index of the last displayed search result
+   */
+  public int getEndIndex(String beginIndex, String items) {
+    int retval = Integer.parseInt(beginIndex) + Integer.parseInt(items) - 1;
+    final int resultcount = getRelevantResults().size();
+    if (retval > resultcount) {
+      return resultcount;
     }
 
-    /**
-     * @return true when there is a page before the one currently displayed, that is, when <code>beginIndex > 1</code>
-     */
-    public boolean hasPrevious(String beginIndex)
-    {
-        return Integer.parseInt(beginIndex) > 1;
-    }
+    return retval;
+  }
 
-    /**
-     * @return the value to be used for the firstIndex URL parameter to build a link pointing to the next page of
-     *         results
-     */
-    public int getNextIndex(String beginIndex, String items)
-    {
-        final int itemCount = Integer.parseInt(items);
-        final int resultcount = getRelevantResults().size();
-        int retval = Integer.parseInt(beginIndex) + itemCount;
+  /**
+   * Helper method for use in velocity templates, takes string values instead of
+   * ints. See {@link #getResults(int,int)}.
+   */
+  public List<SearchResult> getResults(String beginIndex, String items) {
+    return getResults(Integer.parseInt(beginIndex), Integer.parseInt(items));
+  }
 
-        return retval > resultcount ? (resultcount - itemCount + 1) : retval;
-    }
+  /**
+   * Returns a list of search results. According to beginIndex and endIndex,
+   * only a subset of the results is returned. To get the first ten results, one
+   * would use beginIndex=1 and items=10.
+   * 
+   * @param beginIndex
+   *          1-based index of first result to return.
+   * @param items
+   *          number of items to return
+   * @return List of SearchResult instances starting at <code>beginIndex</code>
+   *         and containing up to <code>items</code> elements.
+   */
+  public List<SearchResult> getResults(int beginIndex, int items) {
+    final int listStartIndex = beginIndex - 1;
+    final int listEndIndex = listStartIndex + items;
+    int resultcount = 0;
+    List<SearchResult> relResults = this.relevantResults;
+    if (relResults == null) {
+      relResults = new ArrayList<SearchResult>();
+      TopDocs docs = this.results.topDocs();
+      String database = this.context.getDatabase();
+      try {
+        for (int i = 0; i < docs.scoreDocs.length; i++) {
+          SearchResult result = null;
+          try {
+            result = new SearchResult(this.searcher.doc(docs.scoreDocs[i].doc),
+                docs.scoreDocs[i].score, this.xwiki);
 
-    /**
-     * @return the value to be used for the firstIndex URL parameter to build a link pointing to the previous page of
-     *         results
-     */
-    public int getPreviousIndex(String beginIndex, String items)
-    {
-        int retval = Integer.parseInt(beginIndex) - Integer.parseInt(items);
+            this.context.setDatabase(result.getWiki());
 
-        return 0 < retval ? retval : 1;
-    }
-
-    /**
-     * @return the index of the last displayed search result
-     */
-    public int getEndIndex(String beginIndex, String items)
-    {
-        int retval = Integer.parseInt(beginIndex) + Integer.parseInt(items) - 1;
-        final int resultcount = getRelevantResults().size();
-        if (retval > resultcount) {
-            return resultcount;
-        }
-
-        return retval;
-    }
-
-    /**
-     * Helper method for use in velocity templates, takes string values instead of ints. See
-     * {@link #getResults(int,int)}.
-     */
-    public List<SearchResult> getResults(String beginIndex, String items)
-    {
-        return getResults(Integer.parseInt(beginIndex), Integer.parseInt(items));
-    }
-
-    /**
-     * Returns a list of search results. According to beginIndex and endIndex, only a subset of the results is returned.
-     * To get the first ten results, one would use beginIndex=1 and items=10.
-     * 
-     * @param beginIndex 1-based index of first result to return.
-     * @param items number of items to return
-     * @return List of SearchResult instances starting at <code>beginIndex</code> and containing up to
-     *         <code>items</code> elements.
-     */
-    public List<SearchResult> getResults(int beginIndex, int items)
-    {
-        final int listStartIndex = beginIndex - 1;
-        final int listEndIndex = listStartIndex + items;
-        int resultcount = 0;
-        List<SearchResult> relResults = this.relevantResults;
-        if (relResults == null) {
-            relResults = new ArrayList<SearchResult>();
-            TopDocs docs = this.results.topDocs();
-            String database = this.context.getDatabase();
-            try {
-                for (int i = 0; i < docs.scoreDocs.length; i++) {
-                    SearchResult result = null;
-                    try {
-                        result = new SearchResult(this.searcher.doc(docs.scoreDocs[i].doc), docs.scoreDocs[i].score,
-                            this.xwiki);
-
-                        this.context.setDatabase(result.getWiki());
-
-                        String pageName = null;
-                        if (result.isWikiContent()) {
-                            pageName = result.getWiki() + ":" + result.getSpace() + "." + result.getName();
-                        }
-                        if (result.isWikiContent() && this.xwiki.exists(pageName)
-                            && this.xwiki.checkAccess(pageName, "view")) {
-                            if (resultcount >= listStartIndex) {
-                                relResults.add(result);
-                            }
-                            resultcount++;
-                            if (resultcount == listEndIndex) {
-                                return relResults;
-                            }
-                        }
-                    } catch (Exception e) {
-                        LOGGER.error("error getting search result", e);
-                    }
-                }
-            } finally {
-                this.context.setDatabase(database);
+            String pageName = null;
+            if (result.isWikiContent()) {
+              pageName = result.getWiki() + ":" + result.getSpace() + "."
+                  + result.getName();
             }
-
-            return relResults;
-        } else {
-            resultcount = getRelevantResults().size();
-
-            return getRelevantResults()
-                .subList(listStartIndex, listEndIndex < resultcount ? listEndIndex : resultcount);
+            if (result.isWikiContent() && this.xwiki.exists(pageName)
+                && this.xwiki.checkAccess(pageName, "view")) {
+              if (resultcount >= listStartIndex) {
+                relResults.add(result);
+              }
+              resultcount++;
+              if (resultcount == listEndIndex) {
+                return relResults;
+              }
+            }
+          } catch (Exception e) {
+            LOGGER.error("error getting search result", e);
+          }
         }
-    }
+      } finally {
+        this.context.setDatabase(database);
+      }
 
-    /**
-     * @return all search results in one list.
-     */
-    public List<SearchResult> getResults()
-    {
-        return getRelevantResults();
-    }
+      return relResults;
+    } else {
+      resultcount = getRelevantResults().size();
 
-    /**
-     * @return total number of searchresults the user is allowed to view
-     */
-    public int getHitcount()
-    {
-        return getRelevantResults().size();
+      return getRelevantResults().subList(listStartIndex,
+          listEndIndex < resultcount ? listEndIndex : resultcount);
     }
+  }
 
-    /**
-     * @return total number of searchresults including unallowed items
-     */
-    public int getTotalHitcount()
-    {
-        return this.results.getTotalHits();
-    }
+  /**
+   * @return all search results in one list.
+   */
+  public List<SearchResult> getResults() {
+    return getRelevantResults();
+  }
+
+  /**
+   * @return total number of searchresults the user is allowed to view
+   */
+  public int getHitcount() {
+    return getRelevantResults().size();
+  }
+
+  /**
+   * @return total number of searchresults including unallowed items
+   */
+  public int getTotalHitcount() {
+    return this.results.getTotalHits();
+  }
 }
