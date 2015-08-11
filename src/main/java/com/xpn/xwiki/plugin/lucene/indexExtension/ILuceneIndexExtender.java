531c7d550a0fa8c19b6a9c540eb8f824feba4ff8
Marc Sladek <marc.sladek@synventis.com>
2015-08-11 12:33:47 +0200

    javadoc added to ILuceneIndexExtender

--- src/main/java/com/xpn/xwiki/plugin/lucene/indexExtension/ILuceneIndexExtender.java
+++ src/main/java/com/xpn/xwiki/plugin/lucene/indexExtension/ILuceneIndexExtender.java
@@ -10,8 +10,18 @@ import com.xpn.xwiki.plugin.lucene.AbstractIndexData;
 @ComponentRole
 public interface ILuceneIndexExtender {
 
+  /**
+   * @param data
+   *          about to be indexed
+   * @return true if the provided data is eligible and should be extended by this extender
+   */
   public boolean isEligibleIndexData(AbstractIndexData data);
 
+  /**
+   * @param data
+   *          about to be indexed
+   * @return the fields to be added to the lucene document
+   */
   public Collection<Fieldable> getExtensionFields(AbstractIndexData data);
 
 }
