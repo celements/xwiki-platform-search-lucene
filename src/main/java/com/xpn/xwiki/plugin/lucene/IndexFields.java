673a06e776dade61d2be84aaa902d16eb7dc7251
Marc Sladek <marc@sladek.me>
2020-02-18 13:35:41 +0100

    add parent do indexing (#29)

--- src/main/java/com/xpn/xwiki/plugin/lucene/IndexFields.java
+++ src/main/java/com/xpn/xwiki/plugin/lucene/IndexFields.java
@@ -33,8 +33,7 @@ import org.apache.commons.lang3.time.FastDateFormat;
  */
 public class IndexFields {
 
-  private IndexFields() {
-  }
+  private IndexFields() {}
 
   /**
    * Keyword field, holds a string uniquely identifying a document across the index. this
@@ -73,6 +72,11 @@ public class IndexFields {
    * FullName of the document (example : Main.WebHome)
    */
   public static final String DOCUMENT_FULLNAME = "fullname";
+
+  /**
+   * FullName of the parent
+   */
+  public static final String DOCUMENT_PARENT = "parent";
 
   /**
    * Version of the document
