dcc273bf761377b9dc1660c63611c4f1b0940c5c
Marc Sladek <marc@sladek.me>
2020-04-13 20:53:40 +0200

    fix initialize

--- src/main/java/com/xpn/xwiki/plugin/lucene/IndexRebuilder.java
+++ src/main/java/com/xpn/xwiki/plugin/lucene/IndexRebuilder.java
@@ -156,8 +156,8 @@ public class IndexRebuilder implements LuceneIndexRebuildService {
 
   @Override
   public void initialize(IndexUpdater indexUpdater) {
-    checkState(this.indexUpdater == null, "LuceneIndexRebuildService already initialized");
-    this.indexUpdater.set(checkNotNull(indexUpdater));
+    checkState(this.indexUpdater.compareAndSet(null, checkNotNull(indexUpdater)),
+        "LuceneIndexRebuildService already initialized");
     this.retryInterval.set(getXContext().getWiki().ParamAsLong(PROP_UPDATER_RETRY_INTERVAL, 30));
     this.maxQueueSize.set(getXContext().getWiki().ParamAsLong(PROP_MAX_QUEUE_SIZE, 1000));
     LOGGER.info("LuceneIndexRebuildService initialized");
